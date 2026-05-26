7270f31bfaf83c585f5261d48175c162f761d755
Edwin Rincon <edwindavid3@gmail.com>
2026-05-26 10:45:10 -0500

    conexion db V12

--- cda-backend/src/config/db.js
+++ cda-backend/src/config/db.js
@@ -1,6 +1,5 @@
 const { PrismaClient } = require('@prisma/client');
 
-// Ya no necesita adaptadores. Prisma leerá el prisma.config.ts automáticamente.
 const prisma = new PrismaClient();
 
 module.exports = prisma;
