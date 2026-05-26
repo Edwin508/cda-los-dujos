bb044742122237ca4240574e28465a4542e61ca6
Edwin Rincon <edwindavid3@gmail.com>
2026-05-26 10:02:25 -0500

    conexion db V9

--- cda-backend/src/config/db.js
+++ cda-backend/src/config/db.js
@@ -6,14 +6,16 @@ const ws = require('ws');
 // 1. Soporte de WebSockets para Linux/Render
 neonConfig.webSocketConstructor = ws;
 
-// 2. La URL blindada
-const URL_SEGURA = "postgresql://neondb_owner:npg_X2HWJzfVyJK1@ep-little-sound-apled613-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require";
+// 2. LA TÁCTICA MAESTRA: Destruimos la variable fantasma que inyecta dotenvx
+delete process.env.DATABASE_URL;
 
-// 3. El adaptador oficial de Neon exigido por Prisma 7
-const pool = new Pool({ connectionString: URL_SEGURA });
+// 3. Al no existir la variable en el sistema, Neon está obligado a leer nuestra URL quemada
+const pool = new Pool({
+  connectionString: "postgresql://neondb_owner:npg_X2HWJzfVyJK1@ep-little-sound-apled613-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require"
+});
+
+// 4. Conectamos Prisma 7 a través de su adaptador obligatorio
 const adapter = new PrismaNeon(pool);
-
-// 4. PrismaClient recibe su adaptador obligatorio
 const prisma = new PrismaClient({ adapter });
 
 module.exports = prisma;
