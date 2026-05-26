64ef9f8a19c9c04c2958b945cfb3c2ef3777bc33
Edwin Rincon <edwindavid3@gmail.com>
2026-05-26 11:09:45 -0500

    Conexion Bd V14

--- cda-backend/src/config/db.js
+++ cda-backend/src/config/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: "postgresql://neondb_owner:npg_X2HWJzfVyJK1@ep-little-sound-apled613-pooler.c-7.us-east-1.aws.neon.tech/neondb",
+  connectionString: "postgresql://neondb_owner:npg_X2HWJzfVyjK1@ep-little-sound-apled613-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
   ssl: {
     rejectUnauthorized: false // Esto evita cualquier bloqueo de certificados en Render
   }
