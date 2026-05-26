92b4831f4f30a90dbc3a9f93ee00591d5b4fbf88
Edwin Rincon <edwindavid3@gmail.com>
2026-05-26 10:07:43 -0500

    conexion db V10

--- cda-backend/src/config/db.js
+++ cda-backend/src/config/db.js
@@ -1,21 +1,10 @@
+// 1. Aplastamos cualquier intento de dotenvx inyectando la URL en la memoria central
+process.env.DATABASE_URL = "postgresql://neondb_owner:npg_X2HWJzfVyJK1@ep-little-sound-apled613-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require";
+
+// 2. Importamos el cliente nativo
 const { PrismaClient } = require('@prisma/client');
-const { Pool, neonConfig } = require('@neondatabase/serverless');
-const { PrismaNeon } = require('@prisma/adapter-neon');
-const ws = require('ws');
 
-// 1. Soporte de WebSockets para Linux/Render
-neonConfig.webSocketConstructor = ws;
-
-// 2. LA TÁCTICA MAESTRA: Destruimos la variable fantasma que inyecta dotenvx
-delete process.env.DATABASE_URL;
-
-// 3. Al no existir la variable en el sistema, Neon está obligado a leer nuestra URL quemada
-const pool = new Pool({
-  connectionString: "postgresql://neondb_owner:npg_X2HWJzfVyJK1@ep-little-sound-apled613-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require"
-});
-
-// 4. Conectamos Prisma 7 a través de su adaptador obligatorio
-const adapter = new PrismaNeon(pool);
-const prisma = new PrismaClient({ adapter });
+// 3. El motor nativo en Rust se conecta usando la variable de memoria
+const prisma = new PrismaClient();
 
 module.exports = prisma;
