fa301fc5e2c1d89c0099e12a6cab11eeaaf3a2b3
Edwin Rincon <edwindavid3@gmail.com>
2026-05-26 09:17:28 -0500

    Conexion bd V5

--- cda-backend/src/config/db.js
+++ cda-backend/src/config/db.js
@@ -1,18 +1,7 @@
 const { PrismaClient } = require('@prisma/client');
-const { neonConfig } = require('@neondatabase/serverless');
-const { PrismaNeon } = require('@prisma/adapter-neon');
-const ws = require('ws');
 
-// 1. Mantenemos el soporte de WebSockets para que funcione en Linux/Render
-neonConfig.webSocketConstructor = ws;
-
-// 2. EL GRAN CAMBIO DE PRISMA 7: Adiós al "Pool".
-// La URL quemada va directa e inyectada en el nuevo adaptador de Neon.
-const adapter = new PrismaNeon({
-  connectionString: "postgresql://neondb_owner:npg_X2HWJzfVyJK1@ep-little-sound-apled613-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require"
-});
-
-// 3. Prisma recibe el adaptador con la conexión asegurada
-const prisma = new PrismaClient({ adapter });
+// Prisma nativo usará su propio motor de alto rendimiento.
+// Leerá automáticamente la variable DATABASE_URL de tu panel de Render.
+const prisma = new PrismaClient();
 
 module.exports = prisma;
