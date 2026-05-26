652fd14c64f8fcc760610aab02bb814dd4c8043f
Edwin Rincon <edwindavid3@gmail.com>
2026-05-25 19:32:15 -0500

    conexion db v2

--- cda-backend/src/config/db.js
+++ cda-backend/src/config/db.js
@@ -1,19 +1,10 @@
+// 1. Sobreescribimos la variable de entorno a la fuerza en la memoria RAM de Node
+process.env.DATABASE_URL = "postgresql://neondb_owner:npg_X2HWJzfVyJK1@ep-little-sound-apled613-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require";
+
+// 2. Importamos el Prisma original
 const { PrismaClient } = require('@prisma/client');
-const { Pool, neonConfig } = require('@neondatabase/serverless');
-const { PrismaNeon } = require('@prisma/adapter-neon');
-const ws = require('ws');
 
-// 1. Configuramos Neon para usar WebSockets en Render
-neonConfig.webSocketConstructor = ws;
-
-// 2. FUERZA BRUTA: Quemamos la URL. Adiós para siempre al problema del .env
-const URL_SEGURA = "postgresql://neondb_owner:npg_X2HWJzfVyJK1@ep-little-sound-apled613-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require";
-
-// 3. El pool ahora toma la variable quemada, nunca será undefined
-const pool = new Pool({ connectionString: URL_SEGURA });
-const adapter = new PrismaNeon(pool);
-
-// 4. Prisma 7 recibe su adaptador
-const prisma = new PrismaClient({ adapter });
+// 3. Prisma 7 leerá automáticamente la variable que le acabamos de obligar a ver
+const prisma = new PrismaClient();
 
 module.exports = prisma;
