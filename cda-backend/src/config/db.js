beacd751999e3231480ee298f6efe142eda6acad
Edwin Rincon <edwindavid3@gmail.com>
2026-05-25 19:16:14 -0500

    conexion bd

--- cda-backend/src/config/db.js
+++ cda-backend/src/config/db.js
@@ -3,15 +3,17 @@ const { Pool, neonConfig } = require('@neondatabase/serverless');
 const { PrismaNeon } = require('@prisma/adapter-neon');
 const ws = require('ws');
 
-// 1. Configuramos Neon para usar WebSockets (el estándar en servidores como Render)
+// 1. Configuramos Neon para usar WebSockets en Render
 neonConfig.webSocketConstructor = ws;
 
-// 2. Render inyecta esta variable de forma nativa e infalible desde el segundo cero
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+// 2. FUERZA BRUTA: Quemamos la URL. Adiós para siempre al problema del .env
+const URL_SEGURA = "postgresql://neondb_owner:npg_X2HWJzfVyJK1@ep-little-sound-apled613-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require";
+
+// 3. El pool ahora toma la variable quemada, nunca será undefined
+const pool = new Pool({ connectionString: URL_SEGURA });
 const adapter = new PrismaNeon(pool);
 
-// 3. ¡Prisma 7 recibe su adaptador obligatorio!
+// 4. Prisma 7 recibe su adaptador
 const prisma = new PrismaClient({ adapter });
 
 module.exports = prisma;
-
