eb68eda1522c97ebaa9fbb872bff78ffb4b9d76e
Edwin Rincon <edwindavid3@gmail.com>
2026-05-27 16:51:48 -0500

    Aplicacion para Instalar

--- cda-frontend/vite.config.js
+++ cda-frontend/vite.config.js
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      // Archivos estáticos que debe precargar
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'CDA Los Dujos',
+        short_name: 'Los Dujos',
+        description: 'Panel Operativo del Centro de Diagnóstico Automotor',
+        theme_color: '#111827', // Color de la barra de estado del celular (gris muy oscuro)
+        background_color: '#f3f4f6',
+        display: 'standalone', // Esta instrucción es la que oculta el navegador de Chrome
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
+});
